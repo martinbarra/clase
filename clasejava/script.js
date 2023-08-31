9aa46c58f9bd982a510264ea4bdd15b4a03e2bc7
martinbarra <mbarra@ing.ucsc.cl>
2023-08-31 11:50:55 -0400

    clase java

---
+++ clasejava/script.js
@@ -0,0 +1,24 @@
+function hola(){
+
+    alert("hola");
+
+}
+ 
+function color1(){
+
+    document.getElementById("div1").style.backgroundColor = "blue"; 
+    
+}
+ 
+function color2(){
+
+    document.getElementsByClassName("divs")[0].style.backgroundColor = "green"; 
+
+}
+
+function reset(){
+
+    document.getElementById("div1").style.backgroundColor = "red"; 
+
+}
+ 
